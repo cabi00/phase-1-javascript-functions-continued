eb79f44a93c8400cefd919584195a21ad18231cf
vanessa garcia <venessag0908@gmail.com>
2023-03-25 21:47:52 -0600

    functionsContinue

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun (activity = "roller-skate") {
+ return `This Saturday, I want to ${activity}!`;
+   }
+function mondayWork (activity = 'go to the office') {
+return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective (flair = "*") {
+     return function(msn = "special"){
+    //const innerFunct = function(msn = "special"){
+        return `You are ${flair}${msn}${flair}!`
+    }
+    //return innerFunct();
+}
